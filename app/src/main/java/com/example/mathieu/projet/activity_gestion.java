8e2b772a8ff5b049bbcd9c5e1eddcfe8cd4e83bd
georgemathieu <33687645+georgemathieu@users.noreply.github.com>
2018-02-22 16:51:18 +0100

    day1

---
+++ app/src/main/java/com/example/mathieu/projet/activity_gestion.java
@@ -0,0 +1,13 @@
+package com.example.mathieu.projet;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class activity_gestion extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gestion);
+    }
+}
