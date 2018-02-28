cfd658b77ac886f10a775c723ed2de252cfbf5db
georgemathieu <33687645+georgemathieu@users.noreply.github.com>
2018-02-28 15:53:53 +0100

    day2

--- app/src/main/java/com/example/mathieu/projet/activity_gestion.java
+++ app/src/main/java/com/example/mathieu/projet/activity_gestion.java
@@ -1,7 +1,10 @@
 package com.example.mathieu.projet;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class activity_gestion extends Activity{
 
@@ -9,5 +12,32 @@ public class activity_gestion extends Activity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gestion);
+
+        Button btnBack= findViewById(R.id.buttonBack);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button btnGestionMembre= findViewById(R.id.buttonGestionMembre);
+        btnGestionMembre.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), activity_gestionMembres.class);
+                startActivity(intent);
+            }
+        });
+
+        Button btnGestionColoc= findViewById(R.id.buttonGestionColoc);
+        btnGestionColoc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), activity_gestionColoc.class);
+                startActivity(intent);
+            }
+        });
     }
 }
